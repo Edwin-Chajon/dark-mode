d341e6d08af389f297e5f24675a4263fff4e79cf
Edwin Chajon <junchajo@gmail.com>
2020-01-15 17:42:44 -0800

    mvp

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import {useState} from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  });
 
-  return [storedValue];
-};
+    const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue
+})
+
+
+
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
+    return [storedValue, setValue]
+}
